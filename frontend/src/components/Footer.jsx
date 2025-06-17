d0aee7c5ad1d259b71315d583bdf4ac882a076f4
HimanshuPaul <h1503paul@gmail.com>
2025-06-17 16:08:45 +0530

    NavBar created and currently working on login page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 function Footer() {
-  return (
-    <div>
-      <h1>Footer</h1>
-      <p>© 2025 Vahak All rights reserved.</p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Footer;
